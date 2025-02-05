dbcb04bc725ae25170d467d817b2ae24231b4616
unknown <thangkay3009@gmail.com>
2025-02-05 13:31:44 +0700

    setup layout

--- app/src/main/java/com/example/kotlin_room_migration/ui/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_room_migration/ui/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.kotlin_room_migration.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.kotlin_room_migration.R
+import com.example.kotlin_room_migration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var biding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        biding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biding.root)
     }
 }
---
+++ app/src/main/java/com/example/kotlin_room_migration/ui/adapter/UserAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_room_migration.ui.adapter
+
+class UserAdapter {
+}
